cfbe2df22ce9df26fb395b7ec4dbc6cfb9628a2c
Tittaya <tittaya2542@gmail.com>
2019-11-08 02:45:09 +0700

    ad ladder class

--- core/src/main/java/com/mystudio/gamename/Variable.java
+++ core/src/main/java/com/mystudio/gamename/Variable.java
@@ -1,8 +1,8 @@
 package com.mystudio.gamename;
 
 public class Variable {
-    public static float SCREEN_WIDTH = 1500;
-    public static float SCREEN_HEIGHT = 1000;
+    public static int SCREEN_WIDTH = 1500;
+    public static int SCREEN_HEIGHT = 1000;
     public static float GRAVITY = 4;
     public static int MAX_OBJ = 50;
 }
