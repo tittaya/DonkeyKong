56890e7c85d4052c523435785aec0a2a06a58b5c
Langen-Tardigrada <46591485+Langen-Tardigrada@users.noreply.github.com>
2019-11-08 09:49:16 +0700

    screen

--- core/src/main/java/com/mystudio/gamename/DonkeyKongGame.java
+++ core/src/main/java/com/mystudio/gamename/DonkeyKongGame.java
@@ -14,6 +14,7 @@ import java.awt.*;
 public class DonkeyKongGame extends BasicGame  {
 	public static final String GAME_IDENTIFIER = "com.mystudio.gamename";
 
+    //-----------------------------------
 	private Texture texture;
 	private Sprite kongSprite, manSprite, groundSprite, floor1Sprite;
 	private Texture kongGraphic, manGraphic, groundGraphic, floor1Graphic;
