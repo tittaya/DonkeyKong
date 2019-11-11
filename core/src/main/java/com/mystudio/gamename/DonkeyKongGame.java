c19739073324958c8294c0ddc6d5267dcd6ae389
Tittaya <tittaya2542@gmail.com>
2019-11-11 23:36:37 +0700

    jump

--- core/src/main/java/com/mystudio/gamename/DonkeyKongGame.java
+++ core/src/main/java/com/mystudio/gamename/DonkeyKongGame.java
@@ -11,20 +11,22 @@ import static com.mystudio.gamename.Variable.*;
 import java.awt.*;
 
 
-public class DonkeyKongGame extends BasicGame  {
-	public static final String GAME_IDENTIFIER = "com.mystudio.gamename";
+public class DonkeyKongGame extends BasicGame {
+    public static final String GAME_IDENTIFIER = "com.mystudio.gamename";
 
-	private Texture texture;
-	private Sprite kongSprite, manSprite, groundSprite, floor1Sprite;
-	private Texture kongGraphic, manGraphic, groundGraphic, floor1Graphic;
-	private Monkey Kong;
-	private Man man;
-	private Floor ground, floor1;
-	private CollisionBox[] gameObjCollisionBox;
-	private GameObject[] gameObj;
+    private Texture texture;
+    private Sprite kongSprite, manSprite, groundSprite, floor1Sprite;
+    private Texture kongGraphic, manGraphic, groundGraphic, floor1Graphic, bgTexture;
+    private Monkey Kong;
+    private Man man;
+    private Floor ground, floor1;
+    private GameObject[] gameObj;
 
-	@Override
+    @Override
     public void initialise() {
+        bgTexture = new Texture("bg2.png");
+
+
         kongGraphic = new Texture("Kong1.png");
         kongSprite = new Sprite(kongGraphic);
         Kong = new Monkey(kongSprite);
@@ -35,56 +37,41 @@ public class DonkeyKongGame extends BasicGame  {
 
         groundGraphic = new Texture("Ground3.png");
         groundSprite = new Sprite(groundGraphic);
-        ground = new Floor(groundSprite,0,800, 1300, 200);
+        ground = new Floor(groundSprite, 0, 800, 1300, 200);
 
         floor1Graphic = new Texture("floor2.png");
         floor1Sprite = new Sprite(floor1Graphic);
-        floor1 = new Floor(floor1Sprite,0,300,800,100);
+        floor1 = new Floor(floor1Sprite, 0, 300, 800, 100);
 
-        gameObjCollisionBox = new CollisionBox[MAX_OBJ];
         gameObj = new GameObject[MAX_OBJ];
-        gameObjCollisionBox[0] = ground.getCollisionBox();
-//        gameObjCollisionBox[1] = floor1.getCollisionBox();
-        gameObj[0] = ground;
-//        gameObj[1] = floor1;
 
+        gameObj[0] = ground;
+        gameObj[1] = floor1;
 
         MyInputProcessor inputProcessor = new MyInputProcessor(Kong, man);
         Gdx.input.setInputProcessor(inputProcessor);
 
 
     }
-    
+
     @Override
     public void update(float delta) {
         Kong.update();
         man.update();
-        Kong.monkeyMove();
-        man.manMove();
-        for(int i=0; i<MAX_OBJ ; i++) {
-            if(gameObj[i]!= null) {
-                GameObject temp = gameObj[i];
-                if (checkCollision(Kong.getMonkeyBox(), temp.getCollisionBox())) {
-                    Kong.setOnFloor(true);
-                    Kong.monkeyMove();
-                }
-                if (!checkCollision(Kong.getMonkeyBox(), temp.getCollisionBox())) {
-                    Kong.setOnFloor(false);
-                    Kong.monkeyMove();
-                }
-                if(checkCollision(man.getManBox(), temp.getCollisionBox())){
-                    man.setOnFloor(true);
-                    man.manMove();
-                }
-                if(!checkCollision(man.getManBox(), temp.getCollisionBox())){
-                    man.setOnFloor(false);
-                    man.manMove();
-                }
-            }
+        if(checkCollision(Kong.getMonkeyBox(),gameObj)){
+            Kong.setOnFloor(true);
+            Kong.setCanJump(true);
+        }else{
+            Kong.setOnFloor(false);
         }
+        Kong.monkeyMove();
+//        Kong.monkeyMove();
+//        man.manMove();
+
+
 
     }
-    
+
     @Override
     public void interpolate(float alpha) {
         Kong.interpolate(alpha);
@@ -93,20 +80,37 @@ public class DonkeyKongGame extends BasicGame  {
 
     @Override
     public void render(Graphics g) {
-        g.setBackgroundColor(Color.WHITE);
+        g.drawTexture(bgTexture, 0, 0);
+//        g.setBackgroundColor(Color.WHITE);
         Kong.render(g);
         man.render(g);
         ground.render(g);
         floor1.render(g);
 
+
     }
 
-    public boolean checkCollision (CollisionBox a,CollisionBox b){
-	    if(a.getPolygon().intersects(b.getPolygon())){ return true; }
-        else return false;
+    public boolean checkCollision(CollisionBox a, GameObject[] b) {
+        for (int i = 0; i < b.length; i++) {
+            GameObject x = b[i];
+            if (x != null) {
+                if(a.intersects(x.getCollisionBox())){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+
+        }
+        return false;
     }
+}
+
+//	    if(a.getPolygon().intersects(b.getPolygon())){ return true; }
+//        else return false;
+//    }
 //    public boolean checkFloor(CollisionBox a,CollisionBox b){
 //	    if((a.getY()+a.getHeight()) >= b.getY()){ return true; }
 //	    else return false;
 //    }
-}
+
--- core/src/main/java/com/mystudio/gamename/Variable.java
+++ core/src/main/java/com/mystudio/gamename/Variable.java
@@ -1,8 +1,8 @@
 package com.mystudio.gamename;
 
 public class Variable {
-    public static int SCREEN_WIDTH = 1980;
-    public static int SCREEN_HEIGHT = 1080;
-    public static float GRAVITY = 4;
+    public static int SCREEN_WIDTH = 1680;
+    public static int SCREEN_HEIGHT = 1936;
+    public static float GRAVITY = 5;
     public static int MAX_OBJ = 100;
 }
